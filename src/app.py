be30702a4a7a5998417d22c3d6641cf73a5618ea
junior <sinvaljunio123@gmail.com>
2023-01-07 13:56:47 -0300

    resolvido bug1

--- src/app.py
+++ src/app.py
@@ -211,17 +211,7 @@ def update_map(date):
       margin=go.layout.Margin(l=0,r=0,t=0,b=0), showlegend=False)
     return fig
 
-@app.callback(
-    Output("location-button", "children"),
-    [Input("choropleth-map", "clickData"), Input("location-button", "n_clicks")]
-)
-def update_location(click_data, n_clicks):
-    changed_id= [p["prop_id"] for p in dash.callback_context.triggered][0]
-    if click_data is not None and changed_id != "location-button.n_clicks":
-        state=click_data["points"][0]["location"]
-        return "{}".format(state)
-    else:
-        return "BRASIL"
+
 
 
 if __name__ =="__main__":
